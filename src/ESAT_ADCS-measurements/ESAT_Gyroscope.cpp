cd02f523a1b3cb0da9d1b54b7e82ce06973ceb7f
Sergio García-Cuevas González <sgarcia@eusoc.upm.es>
2018-09-18 15:44:47 +0200

    ESAT_Gyroscope: use the new Wire aliases.

--- src/ESAT_ADCS-measurements/ESAT_Gyroscope.cpp
+++ src/ESAT_ADCS-measurements/ESAT_Gyroscope.cpp
@@ -25,10 +25,10 @@ void ESAT_GyroscopeClass::begin(const byte fullScaleConfiguration)
 {
   error = false;
 #ifdef ARDUINO_ESAT_ADCS
-  bus = &Wire1;
+  bus = &WireADCS;
 #endif /* ARDUINO_ESAT_ADCS */
 #ifdef ARDUINO_ESAT_OBC
-  bus = &Wire;
+  bus = &WireOBC;
 #endif /* ARDUINO_ESAT_OBC */
   configureRange(fullScaleConfiguration);
   setGain(fullScaleConfiguration);
